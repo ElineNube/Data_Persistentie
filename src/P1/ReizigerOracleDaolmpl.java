d553f836399a8b81d5388ff471b5348358f16510
ElineNube <eline.nube@student.hu.nl>
2018-04-24 21:26:27 +0200

    Inleveropdracht gemaakt

---
+++ src/P1/ReizigerOracleDaolmpl.java
@@ -0,0 +1,45 @@
+package P1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Date;
+
+public class ReizigerOracleDaolmpl implements ReizigerDao{
+
+    private ArrayList<Reiziger> deReizigers = new ArrayList<>();
+
+    public List<Reiziger> findAll() {
+        return this.deReizigers;
+    }
+
+    public List<Reiziger> findByGBdatum(Date GBdatum) {
+        List<Reiziger> resultaat = new ArrayList<>();
+        for (Reiziger r : this.deReizigers) {
+            if (r.getGBdatum()== GBdatum) {
+                resultaat.add(r);
+            }
+        }
+        return resultaat;
+    }
+
+    public Reiziger save(Reiziger reiziger) {
+        if (!deReizigers.contains(reiziger)){
+            deReizigers.add(reiziger);
+        }
+        return reiziger;
+    }
+
+    public Reiziger update(Reiziger reiziger) {
+        return reiziger;
+    }
+
+    public boolean delete(Reiziger reiziger) {
+        boolean resultaat = false;
+
+        if (deReizigers.contains(reiziger)) {
+            deReizigers.remove(reiziger);
+            resultaat = true;
+        }
+        return resultaat;
+    }
+}
